4b6cf5a5ce2013cb795eecd447d6c27101a812dc
Pranav Teegavarapu <46251241+pranavnt@users.noreply.github.com>
2020-08-16 14:15:23 -0700

    Update app.py

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, url_for, request
 from joblib import load
 from jinja2 import Template
-from transformers import pipeline
+#from transformers import pipeline
 import json
 
 app = Flask(__name__)
 
-sentimentAnalysis = pipeline("sentiment-analysis")
+#sentimentAnalysis = pipeline("sentiment-analysis")
 
 @app.route('/factCheck', methods=['POST'])
 def factCheck():
@@ -15,7 +15,7 @@ def factCheck():
         url = request.form['url']
         prediction = clf.predict_proba([url])
 
-        return render_template('popup.html', probability=json.dumps(prediction))
+        return render_template('templates/popup.html', probability=json.dumps(prediction))
         
         #res = sentimentAnalysis(url)[0]
         #return {
