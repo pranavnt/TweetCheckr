ce1ee9967ddd90fc3c8141e6e2647510e5b7eb9e
Pranav Teegavarapu <46251241+pranavnt@users.noreply.github.com>
2020-08-16 13:24:33 -0700

    should work now

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, url_for, request
 from joblib import load
+from transformers import pipeline
 
 app = Flask(__name__)
 
+sentimentAnalysis = pipeline("sentiment-analysis")
 
 @app.route('/factCheck', methods=['POST'])
 def factCheck():
@@ -10,6 +12,12 @@ def factCheck():
         clf = load('clf.joblib')
         url = request.form['url']
         prediction = clf.predict_proba([url])
+        res = sentimentAnalysis(url)[0]
+        return {
+          "prediction": prediction,
+          "label": res['label'], 
+          "score": res['score']
+         }
 
 
 if __name__ == '__main__':
